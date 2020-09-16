9e92c8be2b0b66a0f9335a383805e226c8fae497
milafreckled <60186181+milafreckled@users.noreply.github.com>
2020-09-16 19:49:39 +0300

    Add files via upload

---
+++ arena.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+root = tk.Tk()
+deli = 100           # milliseconds of delay per character
+svar = tk.StringVar()
+labl = tk.Label(root, textvariable=svar, height=10 )
+
+def shif():
+    shif.msg = shif.msg[1:] + shif.msg[0]
+    svar.set(shif.msg)
+    root.after(deli, shif)
+shif.msg = ' Is this an alert, or what? '
+shif()
+labl.pack()
+root.mainloop()
